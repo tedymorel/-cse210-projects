b2cc8c800f8a4b003d7112639cbc7cad6a9597b6
tedymorel <tedymorel92@gmail.com>
2026-03-11 10:58:34 +0000

    add two sentence

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Sandbox Project.");
+        Console.WriteLine("i'm the best one");
+        Console.WriteLine("JE suis un nouveau apprenant dans le c#");
     }
 }
