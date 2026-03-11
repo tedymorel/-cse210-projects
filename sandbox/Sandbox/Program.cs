5eaa627797905820d6bb7ef718e1869bca2b6958
tedymorel <tedymorel92@gmail.com>
2026-03-11 11:33:28 +0000

    add anather sentence

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -7,5 +7,6 @@ class Program
         Console.WriteLine("Hello World! This is the Sandbox Project.");
         Console.WriteLine("i'm the best one");
         Console.WriteLine("JE suis un nouveau apprenant dans le c#");
+        Console.WriteLine("practice is the best way to learn");
     }
 }
