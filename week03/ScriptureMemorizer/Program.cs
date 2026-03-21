d3182c85e72996898010b6eb970d791eebdf668d
tedymorel <tedymorel92@gmail.com>
2026-03-21 12:17:56 +0000

    Assigment 03 PC

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,9 +1,161 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
+// ------------------ WORD CLASS ------------------
+public class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string text)
+    {
+        _text = text;
+        _isHidden = false;
+    }
+
+    public void Hide()
+    {
+        _isHidden = true;
+    }
+
+    public bool IsHidden()
+    {
+        return _isHidden;
+    }
+
+    public string GetDisplayText()
+    {
+        if (_isHidden)
+        {
+            return new string('_', _text.Length);
+        }
+        else
+        {
+            return _text;
+        }
+    }
+}
+
+// ------------------ REFERENCE CLASS ------------------
+public class Reference
+{
+    private string _book;
+    private int _chapter;
+    private int _verseStart;
+    private int _verseEnd;
+
+    // Single verse
+    public Reference(string book, int chapter, int verse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verseStart = verse;
+        _verseEnd = verse;
+    }
+
+    // Verse range
+    public Reference(string book, int chapter, int verseStart, int verseEnd)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verseStart = verseStart;
+        _verseEnd = verseEnd;
+    }
+
+    public string GetDisplayText()
+    {
+        if (_verseStart == _verseEnd)
+        {
+            return $"{_book} {_chapter}:{_verseStart}";
+        }
+        else
+        {
+            return $"{_book} {_chapter}:{_verseStart}-{_verseEnd}";
+        }
+    }
+}
+
+// ------------------ SCRIPTURE CLASS ------------------
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+    private Random _random = new Random();
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = text.Split(' ')
+                    .Select(word => new Word(word))
+                    .ToList();
+    }
+
+    public void HideRandomWords(int count = 3)
+    {
+        var visibleWords = _words.Where(w => !w.IsHidden()).ToList();
+
+        if (visibleWords.Count == 0)
+            return;
+
+        for (int i = 0; i < count; i++)
+        {
+            if (visibleWords.Count == 0)
+                break;
+
+            int index = _random.Next(visibleWords.Count);
+            visibleWords[index].Hide();
+            visibleWords.RemoveAt(index);
+        }
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return _words.All(w => w.IsHidden());
+    }
+
+    public string GetDisplayText()
+    {
+        string text = string.Join(" ", _words.Select(w => w.GetDisplayText()));
+        return $"{_reference.GetDisplayText()} - {text}";
+    }
+}
+
+// ------------------ PROGRAM CLASS ------------------
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        Reference reference = new Reference("Proverbs", 3, 5, 6);
+        Scripture scripture = new Scripture(reference,
+            "Trust in the Lord with all thine heart and lean not unto thine own understanding in all thy ways acknowledge him and he shall direct thy paths");
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+
+            if (scripture.IsCompletelyHidden())
+            {
+                Console.WriteLine("\nAll words are hidden. Program ending.");
+                break;
+            }
+
+            Console.Write("\nPress Enter to continue or type 'quit': ");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+            {
+                break;
+            }
+
+            scripture.HideRandomWords();
+
+            List<Scripture> scriptures = new List<Scripture>()
+            {
+                new Scripture(new Reference("John", 3, 16), "For God so loved the world..."),
+                new Scripture(new Reference("Psalm", 23, 1), "The Lord is my shepherd...")
+            };
+        }
     }
 }
